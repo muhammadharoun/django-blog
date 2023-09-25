53f4515acfc5bea5e3163f4aad96bfd823ce8223
muhammadharoun <hro19502001@gmail.com>
2023-09-25 15:16:48 +0300

    finish code

--- blogging/admin.py
+++ blogging/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 # a new import
-from .models import Post
+from .models import Post, Category
 
 # and a new admin registration
 admin.site.register(Post)
+admin.site.register(Category)               # <-- Register Category
--- blogging/urls.py
+++ blogging/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from blogging.views import list_view, detail_view
+from blogging.views import stub_view, list_view, detail_view
 
 urlpatterns = [
     path('', list_view, name="blog_index"),
-    path('blog/<int:blog_id>/', detail_view, name="blog_detail"),
+    path('posts/<int:post_id>/',
+         detail_view,  # <-- Change this from stub_view
+         name="blog_detail"),
 ]
