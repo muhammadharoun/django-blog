d2c2c2aa392b38a86f2b78a19326771fea077124
muhammadharoun <hro19502001@gmail.com>
2023-09-27 13:57:42 +0300

    add some custumization to admin page

--- blogging/admin.py
+++ blogging/admin.py
@@ -4,6 +4,19 @@ from django.contrib import admin
 # a new import
 from .models import Post, Category
 
-# and a new admin registration
-admin.site.register(Post)
-admin.site.register(Category)               # <-- Register Category
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ("posts",)
+
+
+class CategoryInLine(admin.TabularInline):
+    model = Category.posts.through
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+        CategoryInLine,
+    ]
+
